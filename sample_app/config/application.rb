6c669aa5edd35e75dc4532428835bdb1a37bcd90
Ernesto de la Cruz <ernesto.delacruz.morales@gmail.com>
2019-04-09 22:04:23 +0000

    application running again

--- sample_app/config/application.rb
+++ sample_app/config/application.rb
@@ -20,9 +20,6 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
